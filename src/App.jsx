836b5b1105e6a96810337a87262f8ec461823d11
agostinolurani-m4g <agostino.lurani@gmail.com>
2025-09-06 03:20:19 -0700

    ultimo

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const EVENT_CONFIG = {
   ],
 };
 
-const SHEETS_CONFIG = { url: "https://script.google.com/macros/s/AKfycbyEKDN2HmxGf7u2J4OMoYPHPRu-e7MkxEOG8qQCG5GrhdRlUJWs2FWeRo-CsvB2TTd9/exec", secret: "Amaro25" };
+const SHEETS_CONFIG = { url: "https://script.google.com/macros/s/AKfycbw_tcxSBg99HWfLSLp4JkE8BlQWd-XgLEMCwWwnRVyUclLI9aw7vqhyAaufND1qqJkD/exec", secret: "Amaro25" };
 async function postSheet(type, payload) {
   try {
     if (!SHEETS_CONFIG.url) return;
