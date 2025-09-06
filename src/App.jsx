949d7bfc9c18d6d6fc21adf40273f2c3de735c0b
agostinolurani-m4g <agostino.lurani@gmail.com>
2025-09-06 02:59:10 -0700

    change deploy

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const EVENT_CONFIG = {
   ],
 };
 
-const SHEETS_CONFIG = { url: "https://script.google.com/macros/s/AKfycbyEKDN2HmxGf7u2J4OMoYPHPRu-e7MkxEOG8qQCG5GrhdRlUJWs2FWeRo-CsvB2TTd9/exec", secret: "Amaro25" };
+const SHEETS_CONFIG = { url: "https://script.google.com/macros/s/AKfycbyh8zO4lSRy9hUehfzcQt09l3ieeMzLPIc7AVwdjy1abx-PUT0GNhKR62OSzfj8Q2v2/exec", secret: "Amaro25" };
 async function postSheet(type, payload) {
   try {
     if (!SHEETS_CONFIG.url) return;
